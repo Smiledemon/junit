8c360b798dbb9662c9f81681363cc2fe249fb4fa
Smiledemon <790333234@qq.com>
2018-03-23 17:48:23 +0800

    junit

---
+++ test/MainTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: xiaopan
+ * Date: 2018-03-23
+ * Time: 下午5:44
+ */
+public class MainTest {
+ @Test
+ public static void main(String[] args) {
+     Assert.assertTrue(true);
+ }
+}
