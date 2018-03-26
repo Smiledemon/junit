3b9c5296e503c806d4fb5792affe4166c4ee8435
Smiledemon <790333234@qq.com>
2018-03-26 16:03:18 +0800

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo rm -r target
-sudo mkdir target
+rm -r target
+mkdir target
 javac -d target src/**/**/*.java
 javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:target -d target/ test/*.java
 cd target/
