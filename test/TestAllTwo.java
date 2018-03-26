d1a7596ffcbc0b6936e75ece448478bd1761edc5
Smiledemon <790333234@qq.com>
2018-03-26 16:49:32 +0800

    test

---
+++ test/TestAllTwo.java
@@ -0,0 +1,15 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: xiaopan
+ * Date: 2018-03-26
+ * Time: 下午4:36
+ */
+@RunWith(org.junit.runners.Suite.class)
+@Suite.SuiteClasses({TestAll.class})
+public class TestAllTwo {
+}
