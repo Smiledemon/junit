66b77f8e8c4b7a12019df7c6e7da9995202ffe09
Smiledemon <790333234@qq.com>
2018-04-08 16:09:22 +0800

    test

--- test/com/xceder/PersonTwo.java
+++ test/com/xceder/PersonTwo.java
@@ -18,6 +18,6 @@ public class PersonTwo {
     @Test
     public void getAge() {
         assertEquals(24,person.getAge());
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
     }
 }
